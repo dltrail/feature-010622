ddbda91c565d35044ed922eeb684320f76ae374b
Dawn Trail <dtrail76@gmail.com>
2022-05-17 13:44:32 +0100

    Build: Basic layout
    New build. Basic layout from previous template. Added in Responsive video handling

---
+++ dist/js/index.js
@@ -0,0 +1,56 @@
+const useProductCarousel = () => {
+  $(".product-carousel__carousel").slick({
+    slidesToShow: 4.5,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    draggable: true,
+    dots: false,
+    arrows: true,
+    infinite: false,
+    accessible: true,
+    prevArrow: $(".prev-arrow"),
+    nextArrow: $(".next-arrow"),
+    responsive: [{
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 2.23
+      }
+    }, {
+      breakpoint: 1023,
+      settings: {
+        slidesToShow: 2.5
+      }
+    }, {
+      breakpoint: 1279,
+      settings: {
+        slidesToShow: 4.5
+      }
+    }]
+  });
+};
+
+useProductCarousel();
+let mbVids = document.querySelectorAll(".videoMobile");
+let dtVids = document.querySelectorAll(".videoDesktop");
+
+const handleVideo = () => {
+  if (window.matchMedia("(max-width: 700px)").matches) {
+    mbVids.forEach(mbvid => {
+      mbvid.style.display = "block";
+    });
+    dtVids.forEach(dtvid => {
+      dtvid.style.display = "none";
+    });
+  } else {
+    mbVids.forEach(mbvid => {
+      mbvid.style.display = "none";
+    });
+    dtVids.forEach(dtvid => {
+      dtvid.style.display = "block";
+    });
+  }
+}; // document.addEventListener("ready", handleVideo);
+
+
+handleVideo();
+window.addEventListener("resize", handleVideo);
---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+const gulp = require("gulp");
+const gulpIf = require("gulp-if");
+
+const sass = require("gulp-sass")(require("sass"));
+const uglify = require("gulp-uglify");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("gulp-autoprefixer");
+const sourcemaps = require("gulp-sourcemaps");
+const cssnano = require("gulp-cssnano");
+const babel = require("gulp-babel");
+const browserify = require("gulp-browserify");
+
+gulp.task("watch", async function () {
+  gulp.watch("src/scss/*.scss", gulp.series(["sass"]));
+  gulp.watch("src/js/*.js", gulp.series(["js"]));
+  gulp.watch("src/index.html");
+});
+
+gulp.task("sass", async function () {
+  return gulp.src("src/scss/*.scss").pipe(sass()).pipe(gulp.dest("dist/css"));
+});
+
+gulp.task("processCSS", async function () {
+  gulp
+    .src("dist/css/*.css")
+    .pipe(sourcemaps.init())
+    .pipe(postcss([require("autoprefixer")]))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulpIf("*.css", cssnano()))
+    .pipe(gulp.dest("dist/css"));
+});
+
+gulp.task("js", function () {
+  return gulp
+    .src("src/js/*.js")
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task("uglify", async function () {
+  gulp.src("src/js/*.js").pipe(uglify()).pipe(gulp.dest("dist/js/*.js"));
+  gulp.src("src/*.css").pipe(uglify()).pipe(gulp.dest("dist/css"));
+});
+
+gulp.task("js", function () {
+  return gulp.src("src/js/*.js").pipe(babel()).pipe(gulp.dest("dist/js"));
+});
+
+gulp.task("processJS", async function () {
+  return gulp
+    .src("dist/js/*.js")
+    .pipe(browserify())
+    .pipe(uglify())
+    .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task(
+  "build",
+  gulp.series(["watch", "sass", "processCSS", "js", "processJS"])
+);
